2adc245b84b581bc9529f5004a79917376b79bb3
Marian Lippold <marian.lippold@gmx.de>
2024-01-11 12:03:29 +0100

    some code i wrote reading the body

--- app.go
+++ app.go
@@ -1,29 +1,75 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/angelofallars/htmx-go"
 	"html/template"
+	"io"
 	"net/http"
+	"strings"
 )
 
 func main() {
 
-	filmSelektionHandler := func(w http.ResponseWriter, request *http.Request) {
-		if !htmx.IsHTMX(request) {
+	validierungsHandler := func(w http.ResponseWriter, r *http.Request) {
+		if !htmx.IsHTMX(r) {
 			fmt.Fprintf(w, "keine HTMX-Anfrage, kein Content!")
 			return
 		}
 
-		dat := template.Must(template.ParseFiles("C:\\Users\\maria\\GolandProjects\\awesomeProject\\routes\\filmselektion.html"))
-		dat.Execute(w, nil)
+		// create new context for cloned request
+		ctx := context.WithValue(context.Background(), "key", "value")
+
+		// clone request to access it freely
+		requestClone := r.Clone(ctx) // we need a context here
+
+		// read cloned request // byte array b
+		b, _ := io.ReadAll(requestClone.Body)
+
+		// alternative body cloning i found
+		// seems bugged or sth
+		body, err := r.GetBody()
+		if err != nil {
+			return
+		}
+
+		defer body.Close()
+		buf := make([]byte, len(b))
+		n, err := body.Read(buf)
+
+		fmt.Printf("%s\n", buf[:n])
+
+		// copy the byte array from the request somewhere
+
+		// body := b
+
+		// body = string(body)
+
+		alleFilme := strings.Split(string(b), "=")
+
+		alleFilme = strings.SplitN(alleFilme[1], "%5Cn", -1)
+		fmt.Println(alleFilme)
+
+		// currently there is no newline character inside the list
+		// reading the bytes from the body, we probably need a better string conversion.
+
 	}
 
 	multiplexer := http.NewServeMux()
 	multiplexer.HandleFunc("/glücksrad", startPageHandler)
 	multiplexer.HandleFunc("/filmselektion", filmSelektionHandler)
+	multiplexer.HandleFunc("/validierung", validierungsHandler)
 
 	http.ListenAndServe(":8080", multiplexer)
+
+}
+
+func validierungsHandler(w http.ResponseWriter, r *http.Request) {
+	if !htmx.IsHTMX(r) {
+		fmt.Fprintf(w, "keine HTMX-Anfrage, kein Content!")
+		return
+	}
 
 }
 
@@ -35,8 +81,6 @@ func filmSelektionHandler(w http.ResponseWriter, request *http.Request) {
 
 	dat := template.Must(template.ParseFiles("C:\\Users\\maria\\GolandProjects\\awesomeProject\\routes\\filmselektion.html"))
 	dat.Execute(w, nil)
-
-	fmt.Fprintf(w, "HTMX-Anfrage")
 }
 
 func startPageHandler(w http.ResponseWriter, r *http.Request) {
