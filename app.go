23d009af543f95fb41b733b4d936d1f5323b2b9c
Marian Lippold <marian.lippold@gmx.de>
2024-01-11 20:13:02 +0100

    database retrieval

--- app.go
+++ app.go
@@ -5,57 +5,157 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/angelofallars/htmx-go"
+	_ "github.com/mattn/go-sqlite3"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
+var db, _ = Connect()
+var (
+	name string
+)
+
 func main() {
+	setupLogging()
 
-	validierungsHandler := func(w http.ResponseWriter, r *http.Request) {
-		if !htmx.IsHTMX(r) {
-			fmt.Fprintf(w, "keine HTMX-Anfrage, kein Content!")
-			return
-		}
-
-		// create new context for cloned request
-		ctx := context.WithValue(context.Background(), "key", "value")
-
-		// clone request to access it freely
-		requestClone := r.Clone(ctx) // we need a context here
-
-		// read cloned request // byte array b
-		// request.Body.src.R.buf -> []uint8 => string
-		b, _ := io.ReadAll(requestClone.Body)
-
-		tmp := strings.Split(string(b), "=")
-
-		tmp2 := tmp[1]
-
-		tmp2 = strings.ReplaceAll(tmp2, "%5Cn", "\n")
-		tmp2 = strings.ReplaceAll(tmp2, "%20", " ")
-		tmp2 = strings.ReplaceAll(tmp2, "%3F", "?")
-		tmp2 = strings.ReplaceAll(tmp2, "%2C", ",")
-
-		alleFilme := strings.SplitN(tmp2, "\n", -1)
-
-		// write alleFilme to database
-		const database string = "movies.sqlite"
-		db, err := sql.Open("sqlite3", database)
-
-		const createTable string = `CREATE TABLE IF NOT EXISTS movies (
-									id INTEGER NOT NULL PRIMARY KEY,
-									name TEXT NOT NULL );`
-	}
-
+	// setup server routes
 	multiplexer := http.NewServeMux()
 	multiplexer.HandleFunc("/glücksrad", startPageHandler)
 	multiplexer.HandleFunc("/filmselektion", filmSelektionHandler)
 	multiplexer.HandleFunc("/validierung", validierungsHandler)
 
-	http.ListenAndServe(":8080", multiplexer)
+	// setup server
+	err := http.ListenAndServe(":8080", multiplexer)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
+func setupLogging() {
+	// setup logging
+	file, err := os.OpenFile("errorLog.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(file)
+}
+
+func selectMoviesDB() (movieList []string) {
+	// get movie list
+	query := "SELECT movies.name from main.movies;"
+	rows, err := db.Query(query)
+
+	defer rows.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	alleFilme := []string{}
+
+	for rows.Next() {
+		err := rows.Scan(&name)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = rows.Err()
+		alleFilme = append(alleFilme, name)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return alleFilme
+}
+
+func Connect() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "database/movies.sqlite")
+	if err != nil {
+		log.Fatal(err)
+		return nil, nil
+	}
+
+	return db, err
+}
+
+func writeToDatabase(filme []string) {
+	// write alleFilme to database
+
+	query := "BEGIN TRANSACTION;"
+
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatalf("locking failed: %s", err)
+	}
+
+	query = "INSERT INTO 'movies' (name) VALUES (?)"
+
+	for _, movie := range filme {
+
+		insertResult, err := db.ExecContext(context.Background(), query, movie)
+
+		if err != nil {
+			log.Fatalf("insert failed: %s", err)
+		}
+
+		id, err := insertResult.LastInsertId()
+		if err != nil {
+			log.Fatalf("impossible to retrieve last inserted id: %s", err)
+		}
+		log.Printf("inserted id: %d", id)
+	}
+
+	query = "COMMIT;"
+
+	_, err = db.Exec(query)
+	if err != nil {
+		log.Fatalf("commiting failed: %s", err)
+	}
+}
+
+func processRequest(clone *http.Request) []string {
+	b, err := io.ReadAll(clone.Body)
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+
+	tmp := strings.Split(string(b), "=")
+
+	tmp2 := tmp[1]
+
+	tmp2 = strings.ReplaceAll(tmp2, "%5Cn", "\n")
+	tmp2 = strings.ReplaceAll(tmp2, "%20", " ")
+	tmp2 = strings.ReplaceAll(tmp2, "%3F", "?")
+	tmp2 = strings.ReplaceAll(tmp2, "%2C", ",")
+
+	alleFilme := strings.SplitN(tmp2, "\n", -1)
+	fmt.Println(alleFilme)
+
+	for index, _ := range alleFilme {
+		alleFilme[index] = string(alleFilme[index])
+
+		if strings.HasPrefix(alleFilme[index], " ") {
+			alleFilme[index], _ = strings.CutPrefix(alleFilme[index], " ")
+		}
+	}
+	return alleFilme
+	// request.Body.src.R.buf -> []uint8 => string
+}
+
+func setupDatabase() {
+	const createTable string = `CREATE TABLE IF NOT EXISTS 'movies' (
+									id INTEGER NOT NULL PRIMARY KEY,
+									name TEXT NOT NULL );`
+
+	if _, err := db.Exec(createTable); err != nil {
+		log.Fatal(err)
+		return
+	}
 }
 
 func validierungsHandler(w http.ResponseWriter, r *http.Request) {
@@ -64,6 +164,15 @@ func validierungsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// create new context for cloned request
+	ctx := context.WithValue(context.Background(), "key", "value")
+
+	// clone request to access it freely
+	requestClone := r.Clone(ctx) // we need a context here
+
+	alleFilme := processRequest(requestClone)
+
+	writeToDatabase(alleFilme)
 }
 
 func filmSelektionHandler(w http.ResponseWriter, request *http.Request) {
@@ -72,13 +181,16 @@ func filmSelektionHandler(w http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	dat := template.Must(template.ParseFiles("C:\\Users\\maria\\GolandProjects\\awesomeProject\\routes\\filmselektion.html"))
-	dat.Execute(w, nil)
+	template := template.Must(template.ParseFiles("C:\\Users\\maria\\GolandProjects\\awesomeProject\\routes\\filmselektion.html"))
+	err := template.Execute(w, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func startPageHandler(w http.ResponseWriter, r *http.Request) {
 	if !htmx.IsHTMX(r) {
-		// fmt.Fprintf(w, "hello World")
+		// serve website
 		http.ServeFile(w, r, "routes/start.html")
 	}
 
@@ -88,12 +200,20 @@ func startPageHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Basic usage, page refresh
 	writer := htmx.NewResponse().Refresh(true)
-	writer.Write(w)
+	err := writer.Write(w)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// RETARGET response
-	htmx.NewResponse().
+	err = htmx.NewResponse().
 		Reswap(htmx.SwapBeforeEnd). // fine tune swap behavior
 		Retarget("#errors").
 		ReplaceURL("/errors").
 		Write(w)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
